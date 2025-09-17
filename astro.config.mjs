8c28275209103a71123a8ccbb82817e677fdd6fd
blaskoivan <blaskocuenta@gmail.com>
2025-09-17 13:06:04 -0300

    septimo-commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react(), preact()]
 });
