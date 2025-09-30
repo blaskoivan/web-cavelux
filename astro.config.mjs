22bccdc3f7a13d46eae094036a8fefed3013c3f5
blaskoivan <blaskocuenta@gmail.com>
2025-09-30 11:45:18 -0300

    0.8.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.cavelux.com.ar',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), preact()]
+  integrations: [react(), preact(), sitemap()]
 });
